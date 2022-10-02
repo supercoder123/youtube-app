076526ac14bdeeab7bb1fcee1bff6c5508036cd8
ashley.fernandes <ashley.fernandes@upstox.com>
2022-10-03 00:41:51 +0530

    add async keyword

--- src/pages/api/set-videos.ts
+++ src/pages/api/set-videos.ts
@@ -9,10 +9,10 @@ export default async function handler(
   ) {
     if (req.method === "POST") {
         const reorderedVideos: VideoCache[] = JSON.parse(req.body);
-        // reorderedVideos.forEach((video) => {
-        //     client.set(video.id, JSON.stringify({ position: video.newPosition}))
-        // });
-        // fetch('https://api.vercel.com/v1/integrations/deploy/prj_FEhOJurqCDz3pjlSpULMDbCKCU7e/l7P7KZBLKk');
+        reorderedVideos.forEach(async (video) => {
+            await client.set(video.id, JSON.stringify({ position: video.newPosition}))
+        });
+        await fetch('https://api.vercel.com/v1/integrations/deploy/prj_FEhOJurqCDz3pjlSpULMDbCKCU7e/l7P7KZBLKk');
         res.status(200).json(reorderedVideos);
     }
   }
