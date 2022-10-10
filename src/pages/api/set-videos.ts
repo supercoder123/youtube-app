36c7e6a8b88ce48419a614ef5cd78f81d6fb1950
ashley.fernandes <ashley.fernandes@upstox.com>
2022-10-10 20:43:00 +0530

    add batch writes

--- src/pages/api/set-videos.ts
+++ src/pages/api/set-videos.ts
@@ -12,13 +12,19 @@ export default async function handler(
   ) {
     if (req.method === "POST") {
         const reorderedVideos: UpdatedVideoPropsItem[] = JSON.parse(req.body);
-        reorderedVideos.forEach(async (video) => {
-          try {
-            await db.collection('youtube').doc(video.id).set(video);
-          } catch(e) {
-            console.error(e)
-          }
+        const batch = db.batch()
+        reorderedVideos.forEach((video) => {
+          const docRef = db.collection('youtube').doc(video.id);
+          batch.set(docRef, video);
         });
+        batch.commit();
+        // reorderedVideos.forEach(async (video) => {
+        //   try {
+        //     await db.collection('youtube').doc(video.id).set(video);
+        //   } catch(e) {
+        //     console.error(e)
+        //   }
+        // });
         // await fetch('https://api.vercel.com/v1/integrations/deploy/prj_FEhOJurqCDz3pjlSpULMDbCKCU7e/l7P7KZBLKk');
         res.status(200).json(reorderedVideos);
     }
